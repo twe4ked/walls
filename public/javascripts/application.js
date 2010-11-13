c9126db8c18b4ab7acf1120a7451c7da092cd42f
twe4ked <odindutton@gmail.com>
2010-11-13 14:26:50 +0800

    Added jquery + hiding reply forms

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('.formtastic.comment').hide();
+  
+  $('a.reply').click(function() {
+    var status_id = $(this).attr('id')
+    $('#new_comment_status_' + status_id).show();
+  });
+  
+});
