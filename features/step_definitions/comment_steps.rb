b736e9ca5964a81df3194a19083fd68f8d8f0d6f
Jason Weathered <jason@jasoncodes.com>
2010-11-13 19:16:59 +0800

    Fix post comment cuke step.

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -1,6 +1,7 @@
-Then /^I post "([^"]*)" as a comment on a status update$/ do |message|
+Then /^I post "([^"]*)" as a comment on "([^"]*)"$/ do |comment_message, status_message|
+  status = Status.find_by_content status_message
   Given "I am on the home page"
-  When %Q[I fill in "comment_content" with "#{message}"]
-  And %Q[I press "Post Comment"]
+  When %Q[I fill in "comment_content" with "#{comment_message}" within "##{dom_id status}"]
+  And %Q[I press "Post Comment" within "##{dom_id status}"]
   Then %Q[I should see "Comment was successfully created"]
 end
