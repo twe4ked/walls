fc5b7bb9879735d6f2b0aa1c93104c546e53e9c4
Jason Weathered <jason@jasoncodes.com>
2010-11-14 13:30:02 +0800

    Nest statuses and comments under walls.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,13 +1,10 @@
 class StatusesController < BasePostController
-  actions :create, :destroy, :show, :index
-
+  actions :create, :destroy, :show
+  belongs_to :wall
+  
   private
 
   def collection_url
     request.referrer || root_path
-  end
-
-  def end_of_association_chain
-    super.includes(:user, :comments => :user)
   end
 end
