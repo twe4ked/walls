79fa681992c28ea15296445ac6ee7a0fb2392237
twe4ked <odindutton@gmail.com>
2010-11-14 19:19:33 +0800

    Added helper method to format content (auto links and auto paragraphs)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       content_tag :div, content_tag(:div, flash[type], :class => "container"), :class => "flash #{type}" if flash[type].present?
     end.join("\n").html_safe
   end
+  
+  def format_content(text)
+    simple_format(auto_link(text, :html => {:target => '_blank', :rel => 'nofollow'}))
+  end
 end
