aed1629dddae1eec9e3f36338d6ac77e461832ce
twe4ked <odindutton@gmail.com>
2010-11-14 10:11:56 +0800

    Added slide and hide js flashes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,37 @@
 $(function() {
   $('time.timeago').timeago();
   $('textarea').elastic();
-});
-
-$(function() {
+  
+  // Auto hide flashes
+  setTimeout(function() {
+    slideAndHide('.flash');
+  }, 4000);
+  
+  // Hide comments forms on page load
   $('form.comment').hide();
-  $('form.comment').each(function()
-  {
+  // Add links for showing comment forms
+  $('form.comment').each(function() {
     var form = $(this);
     var link = $('<a href="#">Add comment</a>');
     link.wrap('<p class="comment"></p>').parent().insertBefore(form);
-    link.click(function()
-    {
+    link.click(function() {
       form.show().find('textarea').focus();
       $(this).hide();
     });
   });
 });
+
+// Slide and hide flashes
+function slideAndHide(element) {
+  $(element).animate({
+    opacity: 0
+  }, 300, function() {
+    $(this).delay(200).animate({
+      height: '0',
+      padding: '0',
+      border: '0'
+    }, 300, 'swing', function() {
+      $(this).hide();
+    });
+  });
+}
